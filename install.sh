9dd9e1dd8e1e81a7b09fc41815b435d94b183382
stormning <stormning@163.com>
2017-03-23 15:12:40 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#epel
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+
+#docker
+sudo yum remove docker \
+                  docker-common \
+                  container-selinux \
+                  docker-selinux \
+                  docker-engine
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --enable docker-ce-edge
+sudo yum makecache fast
+sudo systemctl start docker
+systemctl enable docker
+
+#docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+#git
+sudo yum install -y git
+
+#nodejs
+yum -y install nodejs npm --enablerepo=epel
