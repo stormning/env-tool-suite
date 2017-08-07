c804d074fb9e9bba0f6614024b0ad9f7d4869c8a
stormning <stormning@163.com>
2017-08-07 10:04:34 +0800

    fecru

---
+++ fecru/crucible_keygen.bat
@@ -0,0 +1,21 @@
+@echo off
+set DIR=%CD%
+set JAR=crucible_keygen.jar
+if not exist "%DIR%\%JAR%" goto noJar
+start javaw -jar %JAR%
+IF ERRORLEVEL 1 goto noJava
+goto end
+:noJar
+echo.
+echo Failed to locate %JAR%.
+echo.
+pause
+goto end
+:noJava
+echo.
+echo Failed to locate java executable!
+echo Please install Java Runtime Environment (JRE) and try again.
+echo.
+pause
+goto end
+:end
