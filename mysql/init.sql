cab619fe3cdc6f69c96a9417c54ffdd104218e52
stormning <stormning@163.com>
2017-08-08 17:23:28 +0800

    bamboo bitbucket jira

--- mysql/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE bitbucket CHARACTER SET utf8 COLLATE utf8_bin;
-GRANT ALL ON bitbucket.* TO ‘bitbucket‘@‘%‘ IDENTIFIED BY ‘bitbucket‘;
-GRANT ALL PRIVILEGES ON bitbucket.* TO ‘bitbucket‘@‘localhost‘ IDENTIFIED BY ‘bitbucket‘;
-FLUSH PRIVILEGES;
