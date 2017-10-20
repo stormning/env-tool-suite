5096c909210a6673577ac3a6546373ff1fd5d94a
stormning <stormning@163.com>
2017-10-20 17:46:38 +0800

    confluence

--- build.sh
+++ build.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
-docker build -t "hub.slyak.com/common" ./software/common
-for file in ./software/*
-do
-    imgName=$(basename $file)
-    if [ $imgName != 'common' ]; then
-        docker build -t "hub.slyak.com/$imgName" ${file}
-    fi
-done
+function buildOne (){
+    docker build -t "hub.slyak.com/$1" "$2"
+}
+
+function buildByName (){
+    buildOne  "$1" "./software/$1"
+}
+
+function buildAll (){
+    docker build -t "hub.slyak.com/common" ./software/common
+    for file in ./software/*
+    do
+        imgName=$(basename $file)
+        if [ $imgName != 'common' ]; then
+            buildOne "hub.slyak.com/$imgName" ${file}
+        fi
+    done
+}
+
+if [ $# -gt 0 ]; then
+    for var in $@; do
+        buildByName $var
+    done
+else
+    buildAll
+fi
--- software/confluence/docker-entrypoint.sh
+++ software/confluence/docker-entrypoint.sh
@@ -3,6 +3,9 @@
 # check if the `server.xml` file has been changed since the creation of this
 # Docker image. If the file has been changed the entrypoint script will not
 # perform modifications to the configuration file.
+
+envsubst '${CROWD_BASE_URL}' < "${CONF_WEB}/classes/crowd.properties.tmpl" > "${CONF_WEB}/classes/crowd.properties"
+
 if [ "$(stat --format "%Y" "${CONF_INSTALL}/conf/server.xml")" -eq "0" ]; then
   if [ -n "${X_PROXY_NAME}" ]; then
     xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8090"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${CONF_INSTALL}/conf/server.xml"
